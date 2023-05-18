9d45fead1b5f4c25a213d85e96ceceacd931bd0d
Md. Nurusshafi Evan <76874563+N-Evan@users.noreply.github.com>
2023-05-18 18:26:14 +0600

    Rebind UI & Functionality Base

---
+++ Assets/_Scripts/RebindUI.cs
@@ -0,0 +1,97 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace Studio23.Input.Rebinding
+{
+    public class RebindUI : MonoBehaviour
+    {
+        [SerializeField]
+        private InputActionReference _inputActionReference;
+        [SerializeField]
+        private bool _excludeMouse = true;
+        [Range(0, 10)]
+        [SerializeField]
+        private int _selectedBindingIndex;
+
+        [SerializeField]
+        private InputBinding.DisplayStringOptions _displayStringOptions;
+
+        [Header("Binding Info")]
+        [SerializeField] private InputBinding _inputBinding;
+        private int _bindingIndex;
+
+        [SerializeField] private string _actionName;
+
+        [Header("UI Fields")]
+        [SerializeField]
+        private TextMeshProUGUI _actionText;
+        [SerializeField]
+        private TextMeshProUGUI _rebindText;
+        [SerializeField]
+        private Button _rebindButton;
+        [SerializeField]
+        private Button _resetButton;
+
+        private void OnEnable()
+        {
+            _rebindButton.onClick.AddListener(() => DoRebind());
+            _resetButton.onClick.AddListener(() => ResetBinding());
+
+            if (_inputActionReference != null)
+            {
+                GetBindingInfo();
+                UpdateUI();
+            }
+        }
+
+        private void ResetBinding()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void DoRebind()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void OnValidate()
+        {
+            if (_inputActionReference == null)
+                return;
+            GetBindingInfo();
+            UpdateUI();
+        }
+
+        private void GetBindingInfo()
+        {
+            if (_inputActionReference.action != null)
+                _actionName = _inputActionReference.action.name;
+
+            if (_inputActionReference.action.bindings.Count > _selectedBindingIndex)
+            {
+                _inputBinding = _inputActionReference.action.bindings[_selectedBindingIndex];
+                _bindingIndex = _selectedBindingIndex;
+            }
+        }
+
+        private void UpdateUI()
+        {
+            if (_actionText != null)
+                _actionText.text = _actionName;
+
+            if (_rebindText != null)
+            {
+                if (Application.isPlaying)
+                {
+                    //Grab from input hook
+                }
+                else
+                {
+                    _rebindText.text = _inputActionReference.action.GetBindingDisplayString(_bindingIndex);
+                }
+            }
+        }
+    }
+}
